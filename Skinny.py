93fbd84b68192505ad01f7bfeebaff90dfd8994b
colalillo <colalillo@gmail.com>
2020-08-20 18:41:34 -0500

    pushing something silly

---
+++ Skinny.py
@@ -0,0 +1,11 @@
+#Skinny Project
+def nobody_cares(n):
+    for n in range (n):
+        print('nobody cares')
+
+def still_nobody_cares(n):
+    x = "still nobody cares\n" * n
+    print (x)
+
+nobody_cares(3)
+still_nobody_cares(4)
